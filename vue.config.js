726385e2515ed69b3c8fd9ec7f5f9e4c285a8f50
koibaiyang <2586056022@qq.com>
2023-05-22 18:56:16 +0800

    打包了项目文件

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,22 @@ const path = require('path')
 // 动态拼接 vant 主题定制的 less 文件位置
 const themePath = path.join(__dirname, './src/vantTheme.less')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports.css = {
-  loaderOptions: {
-    less: {
-      // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
-      lessOptions: {
-        modifyVars: {
-          // 直接覆盖变量
-          // 'nav-bar-background-color': 'pink'
-          // 'border-color': '#eee'
-          // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
-          hack: `true; @import "${themePath}";`
+  transpileDependencies: true,
+  publicPath: './',
+  css: {
+    loaderOptions: {
+      less: {
+        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
+        lessOptions: {
+          modifyVars: {
+            // 直接覆盖变量
+            // 'nav-bar-background-color': 'pink'
+            // 'border-color': '#eee'
+            // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+            hack: `true; @import "${themePath}";`
+          }
         }
       }
     }
   }
-}
+})
